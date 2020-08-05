5423f2fc0169b2c22c42bc2d6ccd57be763b6edb
Anitha kevine <anitha123keke@gmail.com>
2020-08-05 16:05:49 +0200

    init

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('hello');
+
+//var food = prompt('what is your favorite food?');
+//document.getElementById('para').innerHTML= food;
+
+function greeting(yourname){
+   var result= 'Hello'+ ' ' + name;//string concatenation 
+   alert(result);
+   var name = prompt('what is your name?');
+
+
+}
+
+//greeting(name);
+/* while loop and for 
+loop*/
+//while loop
+var a = 1;
+var b = 9;
+var sum = 1+9;
+//document.write(sum)
+document.getElementById("btn").onclick = function() {
+   document.getElementById("para").innerHTML = 'the world is not good'
+}
+/* assigning multiple valiables to one value
+var name = 'anitha kevine', age = 12, married = false;
+alert(name,age,married);*/
+
+/*incrementing and decrementing
+var x = 10;
+alert(x++);
+*/
+/*document.getElementById('btn').onmouseover = function(){
+   alert('this is the thing!')
+};
+*/
+var str = "anitha";
+alert(str.length);
